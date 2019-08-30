dab13d057a39bbae59abdfb2fe5fe5e952148c22
Brittany Heller <brittany.heller@live.com>
2019-08-30 13:05:32 -0700

    Adding some dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks"
+  ],
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  "parser": "babel-eslint",
+}
